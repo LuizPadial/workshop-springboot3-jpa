d9ebf865bab38c95c12a9d2cc844caa891ac6f06
luizPadial <luizpadial10@gmail.com>
2024-03-26 16:38:20 -0300

    User entity Resource

---
+++ src/main/java/com/cursoSpringBoot/curso/UserResource.java
@@ -0,0 +1,30 @@
+package com.cursoSpringBoot.curso;
+
+import com.cursoSpringBoot.curso.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findall(){
+        User u = new User(1L, "Maria", "padial@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

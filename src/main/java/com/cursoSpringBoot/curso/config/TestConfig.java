3c7a66b2afc0618a4c8cc74892735f62083fd413
luizPadial <luizpadial10@gmail.com>
2024-03-30 14:11:52 -0300

    Criado metodo Payment e na classe test um pagamento

--- src/main/java/com/cursoSpringBoot/curso/config/TestConfig.java
+++ src/main/java/com/cursoSpringBoot/curso/config/TestConfig.java
@@ -73,6 +73,12 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T22:53:07Z"),o1 );
+        //pedido associado com pagamento e salvo pedido novamente
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+
     }
 
 
--- src/main/java/com/cursoSpringBoot/curso/entities/Order.java
+++ src/main/java/com/cursoSpringBoot/curso/entities/Order.java
@@ -31,6 +31,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     @JsonManagedReference
     private Set<OrderItem> items = new HashSet<>();
+    //O cascade mapeia para usar o mesmo ID na relação 1 para 1
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
@@ -73,6 +76,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
